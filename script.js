26064502a34aaeee4b0f629281c337eec42bed43
mrhanhh <andikapunya212@mail.com>
2022-07-13 21:43:14 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document
+  .getElementsByClassName("toggler-navbar")[0]
+  .addEventListener("click", toogleClass);
+
+function toogleClass() {
+  document.getElementsByClassName("hamburger-menu")[0].classList.toggle("open");
+  document.getElementsByClassName("head")[0].classList.toggle("open");
+}
